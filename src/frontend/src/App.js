8414380637966289582a37391c63328d497643c8
techie-pro <techiepro6@gmail.com>
2022-08-14 17:50:43 +0530

    Added Homepage and Year selector and stlyed all the pages

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,10 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 import MatchPage from './pages/MatchPage';
 import { TeamPage } from './pages/TeamPage';
+import HomePage from './pages/HomePage';
+
 function App() {
   return (
     <div className='App'>
       <Routes>
+        <Route path='/' element={<HomePage />}></Route>
         <Route path='/teams/:teamName' element={<TeamPage />}></Route>
         <Route
           path='/teams/:teamName/matches/:year'
--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,27 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import { React, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import MatchDetailCard from '../components/MatchDetailCard';
+
+import { YearSelector } from '../components/YearSelector';
+
+import './MatchPage.scss';
 
 const MatchPage = () => {
   const [matches, setMatches] = useState([]);
-  const teamName = 'Delhi Capitals';
+  const { teamName, year } = useParams();
   useEffect(() => {
     const fetchMatches = async () => {
       const response = await fetch(
-        `http://localhost:8080/team/${teamName}/matches?year=2019`
+        `${process.env.REACT_APP_API_ROOT_URL}/team/${teamName}/matches?year=${year}`
       );
       const data = await response.json();
       setMatches(data);
     };
     fetchMatches();
-  }, []);
+  }, [teamName, year]);
+
   return (
-    <>
-      <h2>Match Page</h2>
-      {matches.map((m) => (
-        <MatchDetailCard teamName={teamName} match={m} />
-      ))}
-    </>
+    <div className='MatchPage'>
+      <div className='year-selector'>
+        <h3> Select Year </h3>
+        <YearSelector teamName={teamName} />
+      </div>
+      <div>
+        <h1 className='page-heading'>
+          {teamName} matches in {year}
+        </h1>
+        {matches.map((match) => (
+          <MatchDetailCard key={match.id} teamName={teamName} match={match} />
+        ))}
+      </div>
+    </div>
   );
 };
-
 export default MatchPage;
