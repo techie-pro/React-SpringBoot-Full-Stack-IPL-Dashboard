4ea3ec77926fcfd724e0786436b6ba680ae09e7e
techie-pro <techiepro6@gmail.com>
2022-08-14 16:05:11 +0530

    Added Routing logic to Match page

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import MatchDetailCard from '../components/MatchDetailCard';
 
 const MatchPage = () => {
+  const [matches, setMatches] = useState([]);
+  const teamName = 'Delhi Capitals';
+  useEffect(() => {
+    const fetchMatches = async () => {
+      const response = await fetch(
+        `http://localhost:8080/team/${teamName}/matches?year=2019`
+      );
+      const data = await response.json();
+      setMatches(data);
+    };
+    fetchMatches();
+  }, []);
   return (
-    <div>MatchPage</div>
-  )
-}
+    <>
+      <h2>Match Page</h2>
+      {matches.map((m) => (
+        <MatchDetailCard teamName={teamName} match={m} />
+      ))}
+    </>
+  );
+};
 
-export default MatchPage
+export default MatchPage;
